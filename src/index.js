fe1243720ff09a25e0e5b9eb61d7635fccfce4a3
MarcusVMendes <marcus.mendes89@gmail.com>
2022-05-26 02:27:19 -0300

    Update Readme.md

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root'),
 );
 
